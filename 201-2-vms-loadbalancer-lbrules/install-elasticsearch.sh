ef90b43a26882191a40b111dd00f6596e53f00f1
Mo-GH <34102804+Mo-GH@users.noreply.github.com>
2019-07-10 15:16:27 +0200

    Update install-elasticsearch.sh

--- 201-2-vms-loadbalancer-lbrules/install-elasticsearch.sh
+++ 201-2-vms-loadbalancer-lbrules/install-elasticsearch.sh
@@ -31,7 +31,7 @@ install_es()
 {
     wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
     echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-7.x.list
-    apt update && sudo apt install -y elasticsearch 
+    apt update && sudo apt install --assume-yes elasticsearch 
    
 }
 
